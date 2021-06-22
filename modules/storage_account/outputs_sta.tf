c335c5b23bf5948c8de60461bbce1c138aaf11eb
Francisco Nieto <fnieto@kabel.es>
2021-06-22 08:19:54 +0200

    sta kv optional

--- modules/storage_account/outputs_sta.tf
+++ modules/storage_account/outputs_sta.tf
@@ -4,7 +4,7 @@ output "primary_blob_endpoint" {
 
 #Outputs to KeyVault
 resource "azurerm_key_vault_secret" "output_sta_primary_blob_endpoint" {
-  #count        = var.kv_id != "" ? 1 : 0
+  count        = var.kv_id ? 1 : 0
   name         = "database-server-fqdn"
   value        = azurerm_storage_account.sta.primary_blob_endpoint
   key_vault_id = var.kv_id

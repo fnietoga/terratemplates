af54c1cfa8060fb2ea05836ae23de5884d99df93
Francisco Nieto <fnieto@kabel.es>
2021-06-22 09:05:26 +0200

    outputs to allow multiple instances

--- modules/key_vault/outputs_kv.tf
+++ modules/key_vault/outputs_kv.tf
@@ -1,18 +1,26 @@
 output "id" {
     value = azurerm_key_vault.deploy_kv.id
 }
+output "name" {
+    value = azurerm_key_vault.deploy_kv.name
+}
 output "url" {
     value = azurerm_key_vault.deploy_kv.vault_uri
 }
 
 #Outputs to KeyVault
 resource "azurerm_key_vault_secret" "output_keyvault_id" {
-  name         = "keyvault-id"
+  name         = "keyvault-${azurerm_key_vault.deploy_kv.name}-id"
   value        = azurerm_key_vault.deploy_kv.id
   key_vault_id = azurerm_key_vault.deploy_kv.id
 }
+resource "azurerm_key_vault_secret" "output_keyvault_name" {
+  name         = "keyvault-${azurerm_key_vault.deploy_kv.name}-name"
+  value        = azurerm_key_vault.deploy_kv.name
+  key_vault_id = azurerm_key_vault.deploy_kv.id
+}
 resource "azurerm_key_vault_secret" "output_keyvault_url" {
-  name         = "keyvault-url"
+  name         = "keyvault-${azurerm_key_vault.deploy_kv.name}-url"
   value        = azurerm_key_vault.deploy_kv.vault_uri
   key_vault_id = azurerm_key_vault.deploy_kv.id
 }
--- modules/storage_account/outputs_sta.tf
+++ modules/storage_account/outputs_sta.tf
@@ -1,3 +1,9 @@
+output "id" {
+  value = azurerm_storage_account.sta.id
+}
+output "name" {
+  value = azurerm_storage_account.sta.name
+}
 output "primary_blob_endpoint" {
   value = azurerm_storage_account.sta.primary_blob_endpoint
 }
@@ -33,58 +39,68 @@ output "primary_access_key" {
 }
 
 #Outputs to KeyVault
+resource "azurerm_key_vault_secret" "output_sta_id" {
+  name         = "sta-${azurerm_storage_account.sta.name}-id"
+  value        = azurerm_storage_account.sta.id
+  key_vault_id = var.kv_id
+}
+resource "azurerm_key_vault_secret" "output_sta_name" {
+  name         = "sta-${azurerm_storage_account.sta.name}-name"
+  value        = azurerm_storage_account.sta.name
+  key_vault_id = var.kv_id
+}
 resource "azurerm_key_vault_secret" "output_sta_primary_blob_endpoint" {
-  name         = "sta-primary-blob-endpoint"
+  name         = "sta-${azurerm_storage_account.sta.name}-primary-blob-endpoint"
   value        = azurerm_storage_account.sta.primary_blob_endpoint
   key_vault_id = var.kv_id
 }
 resource "azurerm_key_vault_secret" "output_sta_primary_blob_host" {
-  name         = "sta-primary-blob-host"
+  name         = "sta-${azurerm_storage_account.sta.name}-primary-blob-host"
   value        = azurerm_storage_account.sta.primary_blob_host
   key_vault_id = var.kv_id
 }
 resource "azurerm_key_vault_secret" "output_sta_primary_queue_endpoint" {
-  name         = "sta-primary-queue-endpoint"
+  name         = "sta-${azurerm_storage_account.sta.name}-primary-queue-endpoint"
   value        = azurerm_storage_account.sta.primary_queue_endpoint
   key_vault_id = var.kv_id
 }
 resource "azurerm_key_vault_secret" "output_sta_primary_queue_host" {
-  name         = "sta-primary-queue-host"
+  name         = "sta-${azurerm_storage_account.sta.name}-primary-queue-host"
   value        = azurerm_storage_account.sta.primary_queue_host
   key_vault_id = var.kv_id
 }
 resource "azurerm_key_vault_secret" "output_sta_primary_table_endpoint" {
-  name         = "sta-primary-table-endpoint"
+  name         = "sta-${azurerm_storage_account.sta.name}-primary-table-endpoint"
   value        = azurerm_storage_account.sta.primary_table_endpoint
   key_vault_id = var.kv_id
 }
 resource "azurerm_key_vault_secret" "output_sta_primary_table_host" {
-  name         = "sta-primary-table-host"
+  name         = "sta-${azurerm_storage_account.sta.name}-primary-table-host"
   value        = azurerm_storage_account.sta.primary_table_host
   key_vault_id = var.kv_id
 }
 resource "azurerm_key_vault_secret" "output_sta_primary_file_endpoint" {
-  name         = "sta-primary-file-endpoint"
+  name         = "sta-${azurerm_storage_account.sta.name}-primary-file-endpoint"
   value        = azurerm_storage_account.sta.primary_file_endpoint
   key_vault_id = var.kv_id
 }
 resource "azurerm_key_vault_secret" "output_sta_primary_file_host" {
-  name         = "sta-primary-file-host"
+  name         = "sta-${azurerm_storage_account.sta.name}-primary-file-host"
   value        = azurerm_storage_account.sta.primary_file_host
   key_vault_id = var.kv_id
 }
 resource "azurerm_key_vault_secret" "output_sta_primary_dfs_endpoint" {
-  name         = "sta-primary-dfs-endpoint"
+  name         = "sta-${azurerm_storage_account.sta.name}-primary-dfs-endpoint"
   value        = azurerm_storage_account.sta.primary_dfs_endpoint
   key_vault_id = var.kv_id
 }
 resource "azurerm_key_vault_secret" "output_sta_primary_dfs_host" {
-  name         = "sta-primary-dfs-host"
+  name         = "sta-${azurerm_storage_account.sta.name}-primary-dfs-host"
   value        = azurerm_storage_account.sta.primary_dfs_host
   key_vault_id = var.kv_id
 }
 resource "azurerm_key_vault_secret" "output_sta_primary_access_key" {
-  name         = "sta-primary-access-key"
+  name         = "sta-${azurerm_storage_account.sta.name}-primary-access-key"
   value        = azurerm_storage_account.sta.primary_access_key
   key_vault_id = var.kv_id
 }

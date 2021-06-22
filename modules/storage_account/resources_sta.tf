17a798afd50050ee1736d4bbfddad5a21ca10676
Francisco Nieto <fnieto@kabel.es>
2021-06-22 08:17:57 +0200

    sta outputs

--- modules/storage_account/resources_sta.tf
+++ modules/storage_account/resources_sta.tf
@@ -48,10 +48,3 @@ resource "azurerm_storage_table" "sta_tables" {
   name                 = each.value.name
 }
 
-#Outputs to KeyVault
-resource "azurerm_key_vault_secret" "output_sta_primary_blob_endpoint" {
-  count        = var.kv_id != "" ? 1 : 0
-  name         = "database-server-fqdn"
-  value        = azurerm_storage_account.sta.primary_blob_endpoint
-  key_vault_id = var.kv_id
-}

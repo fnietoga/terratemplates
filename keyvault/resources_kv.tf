c6d2e12ebab95a21da355be180f8b0a681fb9f52
Francisco Nieto <fnieto@kabel.es>
2021-06-22 05:04:05 +0200

    keyvault template v1.0

---
+++ keyvault/resources_kv.tf
@@ -0,0 +1,66 @@
+#Deployment current context
+data "azurerm_client_config" "current" {
+}
+#Deployment current public IP
+data "http" "myip" {
+  url = "http://ipv4.icanhazip.com"
+}
+
+# Key Vault to store deploy sensitive and iac outputs
+resource "azurerm_key_vault" "deploy_kv" {
+  name                = var.kv_name
+  location            = var.azure_location
+  resource_group_name = var.resource_group_name
+  tenant_id           = data.azurerm_client_config.current.tenant_id
+  sku_name            = var.kv_sku_name
+
+  enabled_for_deployment          = var.kv_enabled_for_deployment
+  enabled_for_disk_encryption     = var.kv_enabled_for_disk_encryption
+  enabled_for_template_deployment = var.kv_enabled_for_template_deployment
+  enable_rbac_authorization       = var.kv_enable_rbac_authorization
+  purge_protection_enabled        = var.kv_purge_protection_enabled
+  soft_delete_retention_days      = var.kv_soft_delete_retention_days
+
+  access_policy {
+    tenant_id               = data.azurerm_client_config.current.tenant_id
+    object_id               = data.azurerm_client_config.current.object_id
+    key_permissions         = ["get", "list", "create", "update", "verify", "delete", "purge"]
+    certificate_permissions = ["get", "list", "create", "update", "delete", "purge", "recover"]
+    secret_permissions      = ["get", "list", "set", "delete", "purge", "recover", "backup", "restore"]
+    storage_permissions     = ["get", "list", "set", "update", "regeneratekey", "delete", "purge"]
+  }
+
+  network_acls {
+    bypass         = "None"
+    default_action = "Deny"
+    ip_rules       = local.kv_always_deploy_ips
+  }
+
+  tags = var.tags
+
+}
+
+
+resource "azurerm_key_vault_access_policy" "deploy_kv_acl" {
+  for_each = { for acl in var.kv_access_policies : acl.object_id => acl }
+
+  key_vault_id            = azurerm_key_vault.deploy_kv.id
+  tenant_id               = data.azurerm_client_config.current.tenant_id
+  object_id               = each.value.object_id
+  certificate_permissions = each.value.certificate_permissions
+  secret_permissions      = each.value.secret_permissions
+  key_permissions         = each.value.key_permissions
+  storage_permissions     = each.value.storage_permissions
+}
+
+#Outputs to KeyVault
+resource "azurerm_key_vault_secret" "output_keyvault_id" {
+  name         = "keyvault-id"
+  value        = azurerm_key_vault.deploy_kv.id
+  key_vault_id = azurerm_key_vault.deploy_kv.id
+}
+resource "azurerm_key_vault_secret" "output_keyvault_url" {
+  name         = "keyvault-url"
+  value        = azurerm_key_vault.deploy_kv.vault_uri
+  key_vault_id = azurerm_key_vault.deploy_kv.id
+}
---
+++ keyvault/variables_kv.tf
@@ -0,0 +1,104 @@
+####REQUIRED Input Variables
+variable "resource_group_name" {
+  type        = string
+  description = "(Required) The name of the resource group in which to create the Key Vault. Changing this forces a new resource to be created."
+}
+variable "kv_name" {
+  type        = string
+  description = " (Required) Specifies the name of the Key Vault. Changing this forces a new resource to be created."
+  validation {
+    condition = can(regex("^[a-zA-Z]{1}[a-zA-Z0-9-]{2,23}$",var.kv_name))
+    error_message = "Vault name must only contain alphanumeric characters and dashes and cannot start with a number, and must be between 3-24 alphanumeric characters."
+  }
+}
+
+####OPTIONAL Input Variables
+variable "azure_location" {
+  type        = string
+  description = "(Optional) Specifies the supported Azure location where the resource exists. Changing this forces a new resource to be created. Defaults to westeurope."
+  default     = "westeurope"
+  validation {
+    condition     = index(["westeurope", "northeurope"], var.azure_location) == 0
+    error_message = "The location specified must be one of the allowed values (westeurope, northeurope)."
+  }
+} 
+
+variable "tags" {
+  type = map
+  description = "(Optional) A mapping of tags which should be assigned to the Resource Group."
+  default= {}
+} 
+
+
+variable "kv_sku_name" {
+  type        = string
+  description = "(Optional) The Name of the SKU used for this Key Vault. Possible values are standard and premium. Defaults to standard."
+  default     = "standard"
+  validation {
+    condition     = index(["standard", "premium"], var.kv_sku_name) == 0
+    error_message = "The Name of the SKU specified must be one of the allowed values (standard, premium)."
+  }
+}
+
+variable "kv_enabled_for_deployment" {
+  type        = bool
+  description = "(Optional) Boolean flag to specify whether Azure Virtual Machines are permitted to retrieve certificates stored as secrets from the key vault. Defaults to false."
+  default     = false
+}
+
+variable "kv_enabled_for_disk_encryption" {
+  type        = bool
+  description = "(Optional) Boolean flag to specify whether Azure Disk Encryption is permitted to retrieve secrets from the vault and unwrap keys. Defaults to false."
+  default     = false
+}
+
+variable "kv_enabled_for_template_deployment" {
+  type        = bool
+  description = "(Optional) Boolean flag to specify whether Azure Resource Manager is permitted to retrieve secrets from the key vault. Defaults to false."
+  default     = false
+}
+
+variable "kv_enable_rbac_authorization" {
+  type        = bool
+  description = "(Optional) Boolean flag to specify whether Azure Key Vault uses Role Based Access Control (RBAC) for authorization of data actions. Defaults to false."
+  default     = false
+}
+
+variable "kv_purge_protection_enabled" {
+  type        = bool
+  description = "(Optional) Is Purge Protection enabled for this Key Vault? Defaults to false."
+  default     = false
+}
+
+variable "kv_soft_delete_retention_days" {
+  type        = number
+  description = "(Optional) The number of days that items should be retained for once soft-deleted. This value can be between 7 and 90 (the default) days"
+  default     = 90
+  validation {
+    condition     = (
+      var.kv_soft_delete_retention_days >= 7 &&
+      var.kv_soft_delete_retention_days <= 90
+    )
+    error_message = "The number of days of retention must be between 7 and 90 days."
+  }
+}
+
+variable "kv_access_policies" {
+  type = list(object({
+    object_id               = string
+    certificate_permissions = optional(list(string))
+    key_permissions         = optional(list(string))
+    secret_permissions      = optional(list(string))
+    storage_permissions     = optional(list(string))
+  }))
+  description = "Object collection with information for additional access policies access to be applied to the Deployment Key Vault."
+  default = []
+}
+
+# Local variables used to reduce repetition 
+locals {
+  #kv_name = "${lower(var.app_name)}-kv-${lower(var.environment)}"
+  kv_always_deploy_ips = [
+    "${chomp(data.http.myip.body)}/32" != "" ? "${chomp(data.http.myip.body)}/32" : null
+  ]
+}
